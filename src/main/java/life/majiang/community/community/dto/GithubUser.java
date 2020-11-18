e43f3b8a3b4d2500357b0bd7ba7565b5fda7d795
kjhanser <1398664686@qq.com>
2020-11-18 22:34:56 +0800

    添加了README
    再次修改
    
    获取了用户信息 通过github的accssToken前面步骤先获取授权在获取code 获kj取code通过code获取accssToken 通过accssToken获取用户信息
    
    后面连接了数据库mysql 成功的拿到了github user的信息 并插入数据库mysql中 使用mybatis
    
    发布功能实现可以向question表里插入数据

--- src/main/java/life/majiang/community/community/dto/GithubUser.java
+++ src/main/java/life/majiang/community/community/dto/GithubUser.java
@@ -1,15 +1,15 @@
 package life.majiang.community.community.dto;
 
 public class GithubUser {
-    private long id;
+    private Long id;
     private String name;
     private String bio;
 
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
